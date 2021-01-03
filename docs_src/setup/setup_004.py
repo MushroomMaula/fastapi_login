eec54cbcb37aa4645455a3efe4d6d0493b5cfb96
MushroomMaula <maxrd79@gmail.com>
2021-01-03 14:45:21 +0100

    Add setup_004.py

---
+++ docs_src/setup/setup_004.py
@@ -0,0 +1,18 @@
+DB = {
+    'users': {
+        'johndoe@mail.com': {
+            'name': 'John Doe',
+            'password': 'hunter2'
+        }
+    }
+}
+
+
+def query_user(user_id: str):
+    """
+    Get a user from the db
+    :param user_id: E-Mail of the user
+    :return: None or the user object
+    """
+    return DB['users'].get(user_id)
+
