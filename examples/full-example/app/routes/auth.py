ecdae337ad8bb1dcea6a59bd138bf1ce46c0212f
Max Rausch-Dupont <maxrd79@gmail.com>
2021-10-11 13:35:32 +0200

    Implement login route

---
+++ examples/full-example/app/routes/auth.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login.exceptions import InvalidCredentialsException
+from sqlalchemy.orm import Session
+
+from app.db import get_session
+from app.db.actions import get_user_by_name
+from app.security import verify_password, manager
+
+router = APIRouter(
+    prefix="/auth"
+)
+
+
+@router.get('/login')
+def login(form_data: OAuth2PasswordRequestForm, db: Session = Depends(get_session)):
+    """
+    Logs in the user provided by form_data.username and form_data.password
+    """
+    user = get_user_by_name(form_data.username)
+    if user is None:
+        raise InvalidCredentialsException
+
+    if not verify_password(form_data.password, user.password):
+        raise InvalidCredentialsException
+
+    token = manager.create_access_token(data={'sub': user.username})
+    return {'token': f"Bearer {token}"}
