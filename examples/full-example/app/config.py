232a7898d85147ebe434cfa01238a20217eef120
Max Rausch-Dupont <maxrd79@gmail.com>
2021-10-11 13:35:25 +0200

    Update route

--- examples/full-example/app/config.py
+++ examples/full-example/app/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     project_root: pathlib.Path = root
     secret: str
     db_uri: str = "sqlite+pysqlite:///app.db"
-    token_url: str = "/auth/token"
+    token_url: str = "/auth/login"
 
     class Config:
         env_file = '.env'
