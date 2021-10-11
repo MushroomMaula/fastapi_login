32890ce061bfd633ddabd19b414dac520f621b9a
Max Rausch-Dupont <maxrd79@gmail.com>
2021-10-11 13:45:24 +0200

    Implement create-admin command

--- examples/full-example/main.py
+++ examples/full-example/main.py
@@ -1,7 +1,8 @@
 import os
 import argparse
 from dotenv import dotenv_values
-from app.db import create_tables
+from app.db import create_tables, get_session
+from app.db.actions import create_user
 
 
 def create_secret(_args):
@@ -30,6 +31,12 @@ def create_secret(_args):
                     f.write(f"{key}={value}")
 
 
+def create_admin(_args):
+    session = next(get_session())
+    create_user(args.username, args.password, session, is_admin=True)
+    print("Admin created.")
+
+
 parser = argparse.ArgumentParser(description='CLI to setup our blogging API.')
 subparsers = parser.add_subparsers()
 
@@ -43,7 +50,7 @@ db_parser.set_defaults(func=create_tables)
 admin_parser = subparsers.add_parser('create-admin', help="Creates a admin user with the given password and username.")
 admin_parser.add_argument('username', help="Username of the admin.")
 admin_parser.add_argument('password', help="Password of the amdin.")
-
+admin_parser.set_defaults(func=create_admin)
 
 if __name__ == '__main__':
     args = parser.parse_args()
