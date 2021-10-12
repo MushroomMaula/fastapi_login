1e4fd586375a1e95143428eb16709d065b4e12bf
Max Rausch-Dupont <maxrd79@gmail.com>
2021-10-12 12:08:53 +0200

    Add InvalidUserName exception

--- examples/full-example/app/exceptions.py
+++ examples/full-example/app/exceptions.py
@@ -6,6 +6,11 @@ UsernameAlreadyTaken = HTTPException(
     detail="A user with this name already exists."
 )
 
+InvalidUserName = HTTPException(
+    status_code=400,
+    detail="No user with this name exists."
+)
+
 
 InvalidPermissions = HTTPException(
     status_code=401,
