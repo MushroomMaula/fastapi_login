e00483b0457b065a7a0a9cd806d7a8f5a1c5e134
Max Rausch-Dupont <maxrd79@gmail.com>
2021-10-23 14:00:14 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name="fastapi-login",
-    version="1.7.2",
+    version="1.7.3",
     author="Max Rausch-Dupont",
     author_email="maxrd79@gmail.com",
     descritpion="Flask-Login like package for FastAPI",
